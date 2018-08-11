771e878b27c8dd38ab53e5458fac6287f651b3c0
Rishats <sultanov-rishat@inbox.ru>
2018-08-11 10:11:33 +0600

    laravel-query-detector example error.

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,16 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Work with soft delete and cascade delete.
 Route::get('/users/only-trashed', 'UsersController@onlyTrashed')->name('onlyTrashed');
 Route::get('/users/with-trashed', 'UsersController@withTrashed')->name('withTrashed');
 Route::get('/users/trashed-user/{id}', 'UsersController@showTrashed')->name('showTrashed');
 Route::patch('/users/trashed-user/{id}', 'UsersController@restoreTrashed')->name('restoreTrashed');
 Route::delete('/users/trashed-user/{id}', 'UsersController@destroyPermanently')->name('destroyPermanently');
 Route::resource('users', 'UsersController');
+
+// Work with N+1 Query Detector
+Route::get('/n+1-query-detector', 'nPlusOneQueryDetectorController@index')->name('index');
 
 Auth::routes();
 
